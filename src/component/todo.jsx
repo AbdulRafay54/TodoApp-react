7491a1af34bd486fb75546db3f6a7e57b4178065
AbdulRafay54 <rafayabdul7817@gmail.com>
2024-09-02 07:08:45 +0500

    working on todo.jsx

--- src/component/todo.jsx
+++ src/component/todo.jsx
@@ -1,65 +1,77 @@
-import { useState } from "react";
+import { useRef } from "react";
+import { useState } from "react"
 
-const Todo = () => {
-  const [text, setText] = useState("");
+
+const App = () => {
+
   const [todo, setTodo] = useState([]);
+  const todoValue = useRef();
 
-  function addTodo(event) {
+  const addTodo = (event) => {
     event.preventDefault();
-    if (text.trim()) {
-      setTodo([...todo, text]);
+
+
+
+    if (!todoValue.current.value) {
+      alert("Please Enter Todo!");
     }
+    else {
+      todo.push(todoValue.current.value);
+      setTodo([...todo]);
+      console.log(todo);
+      todoValue.current.value = "";
+    }
+ 
   }
 
-  function deleteTodo(index) {
-    const newTodo = [...todo];
-    newTodo.splice(index, 1); 
-    setTodo(newTodo);
+
+  const deleteTodo = (index) => {
+
+    todo.splice(index, 1);
+    setTodo([...todo]);
+    console.log(todo);
+
   }
 
-  function editTodo(index) {
-    setText(todo[index]); 
-    deleteTodo(index); 
+  const editTodo = (index) => {
+
+    const updatedTodo = prompt("Enter Edit Todo");
+    if (updatedTodo === "") {
+      alert("Try Again!\nPlease Enter new Edited Todo");
+      return
+    }
+
+    todo.splice(index, 1, updatedTodo);
+    setTodo([...todo]);
+    console.log(todo);
   }
+
+  
 
   return (
-    <>
-      <h1>Todo</h1>
-      <form onSubmit={addTodo}>
-        <input
-          type="text"
-          placeholder="Enter text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button type="submit">Add Todo</button>
-      </form>
-      <ul className="todo-list">
-        {todo.map((item, index) => (
-          <div key={index}>
-            <li className="list">
-              {item}
-              <div className="buttons-div">
-                <button
-                  className="delete-btn buttons"
-                  onClick={() => deleteTodo(index)}
-                >
-                  {" "}
-                  Delete
-                </button>
-                <button
-                  className="edit-btn buttons"
-                  onClick={() => editTodo(index)}
-                >
-                  Edit
-                </button>
-              </div>
-            </li>
-          </div>
-        ))}
-      </ul>
-    </>
-  );
-};
+    < >
+      <div className="container">
+        <h1 className="main-heading">Todo App</h1>
+        <form onSubmit={addTodo}>
+          <input type="text" placeholder="Enter Todo" className="todo-inp"ref={todoValue}  />
+          <button type="submit" className="todo-btn">Add Todo</button>
+        </form>
+        <ul className="todo-list">
+          {todo.map((item, index) => {
+            return <div key={index} >
+              <li className="list">{item}
+                <div className="buttons-div">
+                  <button className="delete-btn buttons" onClick={() => deleteTodo(index)}>Delete</button>
+                  <button className="edit-btn buttons" onClick={() => editTodo(index)}>Edit</button>
+                </div>
+              </li>
 
-export default Todo;
+            </div>
+          })}
+        </ul>
+      </div>
+    </ >
+  )
+}
+
+export default App
